84b09b4e33268b1ff3f26514310b1a6903e6a9da
Jakob <polygonman244@gmail.com>
2018-10-26 01:20:55 -0500

    fixed syntax error on window border conditions

--- Alien_Invasion/ship.py
+++ Alien_Invasion/ship.py
@@ -30,7 +30,7 @@ class Ship:
         # Update the center value, not the rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
         
         # Update rect object from self.center
